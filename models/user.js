cc096f0e967f628456741129afa62d60dca373a4
Sheena Kelly <sheena.96@hotmail.com>
2018-03-19 16:28:59 +0000

    Adding the login functions to the Users model.

--- models/user.js
+++ models/user.js
@@ -29,3 +29,17 @@ module.exports.createUser = function (newUser, callback) {
         });
     });
 }
+
+module.exports.getUserByUsername = function (username, callback) {
+    var query = {username: username};
+    User.findOne(query, callback);
+}
+
+module.exports.comparePassword = function(candidatePassword, hash, callback){
+    // Load hash from your DB.
+    bcrypt.compare("candidatePassword", hash, function(err, isMatch) {
+        if(err) throw err;
+        callback(null, isMatch);
+    });
+}
+
