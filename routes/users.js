89fa4b7a06a9ac42f22aa8a6759ef7a1179d4e37
Sheena Kelly <sheena.96@hotmail.com>
2018-03-19 19:23:31 +0000

    Login working

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
+var passport = require('passport'), LocalStrategy = require('passport-local').Strategy;
 
 var User = require('../models/user');
 
@@ -58,13 +57,13 @@ router.post('/register', function(req, res) {
 
 passport.use(new LocalStrategy(
     function(username, password, done) {
-     User.getUserByUsername(username, function(err, user) {
+     User.getUserByUsername(username, function(err, user){
          if(err) throw err;
          if(!user){
              return done(null, false, {message: 'Unknown User'});
          }
 
-         User.comparePassword(password, user.password, function (err, isMatch) {
+         User.comparePassword(password, user.password, function (err, isMatch){
              if(err) throw err;
              if(isMatch){
                  return done(null, user);
@@ -75,9 +74,21 @@ passport.use(new LocalStrategy(
      });
     }));
 
-app.post('/login',
-    passport.authenticate('local', {successRedirect:'/', failureRedirect:'/users/login', failureFlash: true}),
-    function(req, res) {
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.getUserById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+router.post('/login',
+    passport.authenticate('local', {successRedirect:'/',
+                                    failureRedirect:'/users/login',
+                                    failureFlash: true}),
+    function(req, res){
         res.redirect('/');
     });
 
