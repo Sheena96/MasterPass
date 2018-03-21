4ec56505315d3c2cc07e57f0edda86c68bff893d
Sheena Kelly <sheena.96@hotmail.com>
2018-03-21 15:19:10 +0000

    Logout function working.

--- routes/users.js
+++ routes/users.js
@@ -92,4 +92,14 @@ router.post('/login',
         res.redirect('/');
     });
 
+router.get('/logout', function (req, res) {
+    req.logout();
+
+    req.flash('success_msg', 'You are now logged out');
+
+    res.redirect('/users/login');
+
+});
+
+
 module.exports = router;
