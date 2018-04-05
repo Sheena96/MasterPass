06d592b5191f6fa7642a985194a2748a6243149e
Sheena Kelly <sheena.96@hotmail.com>
2018-04-05 18:28:19 +0100

    Inputting and retrieving data working.

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb');
+var assert = require('assert');
+
+var url = 'mongodb://localhost:27017/masterpass';
 
 /* GET home page. */
 router.get('/', ensureAuthenticated, function(req, res) {
@@ -15,5 +19,46 @@ function ensureAuthenticated(req, res, next) {
       res.redirect('/users/login');
     }
 }
+
+router.get('/get-data', function (req, res, next) {
+    var resultArray = [];
+    mongo.connect(url, function (err, db) {
+        assert.equal(null, err);
+        var cursor = db.db('masterpass').collection('passwords').find();
+        cursor.forEach(function (doc, err) {
+            assert.equal(null, err);
+            resultArray.push(doc);
+        }, function() {
+            db.close();
+            res.render('index', {items: resultArray});
+        });
+    });
+});
+
+router.post('/insert', function (req, res, next) {
+    var item = {
+        website: req.body.website,
+        username: req.body.username,
+        password: req.body.password
+    };
+
+    mongo.connect(url, function (err, db) {
+        assert.equal(null, err);// to check if we have an error
+        db.db('masterpass').collection('passwords').insertOne(item, function(err, result) {
+          assert.equal(null, err)  ;
+          console.log('Item inserted');
+          db.close();
+        });
+    });
+    res.redirect('/');
+});
+
+router.post('/update', function (req, res, next) {
+
+});
+
+router.post('/delete', function (req, res, next) {
+
+});
 
 module.exports = router;
